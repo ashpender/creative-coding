da6ae281cf8c0515faa99a20db08c7e1940d2967
ashpender <ash.pender2020@gmail.com>
2024-10-08 04:20:37 -0400

    making the grid

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -5,7 +5,30 @@ function setup() {
   // disable animation
   noLoop();
 }
-
+//based on instructions
 function draw() {
-  background(220);
+background(220)
+translate(100,100);
+//change 8 to 11 to make it look a bit bigger
+for (let x = 0; x < 11; x++){
+  for (let y = 0; y < 11; y++){
+      //changed if from ellipse to arc
+      arc(-20,0,100,100);
+    //based on instructions
+      push();
+      //based on instructions
+      translate(x * 100, y * 100);
+        // Random the main color
+      fill(random(300), random(300), random(300));
+      // Random the stroke color
+      stroke(random(300), random(300), random(300));
+      // Random the stroke thickness, making it out of 8 so there is more variety and the grid is 8 by 8
+      strokeWeight(random(0, 8));
+      //based on instructions
+      ellipse(0,0,100,100);
+      //based on instructions
+      pop();
+   }
+}
+  
 }
