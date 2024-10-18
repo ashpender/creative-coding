2f3b2ee861b1ad70107fdb911ca7ba6e969112cd
ashpender <ash.pender2020@gmail.com>
2024-10-17 21:18:13 -0400

    changes

---
+++ unit2/2.3.emulation - Copy/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // create the canvas
+  createCanvas(1050, 680);
+
+  //colors background(218,218,214) green(44,107,72)
+  // disable animation
+  noLoop();
+}
+
+function draw() {
+ background(218,218,214);
+
+}
